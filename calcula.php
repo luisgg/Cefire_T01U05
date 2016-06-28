8918aa3378e04c20f2ec52b0b60ab7793ce41fad
Luis Garcia <luisgg@gmail.com>
2016-06-28 12:55:33 +0200

    Añadidos index.html, calcula.php y ficheros de proyecto

---
+++ calcula.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Titulo</title>
+    </head>
+    <body>
+        
+        <?php
+        $dividendo_text = $_REQUEST["dividendo"];
+        $divisor_text = $_REQUEST["divisor"];
+        
+        if ($dividendo_text != "") {
+            $dividendo = (int) $dividendo_text;
+            if ($divisor_text != "") {
+                $divisor = (int) $divisor_text;
+                if ($divisor != 0){
+                    $cociente = (int)($dividendo / $divisor);
+                    $resto = $dividendo % $divisor;
+                    echo "Cociente: $cociente <br>";
+                    echo "Resto: $resto";
+                } 
+                
+            }
+        }
+                
+        ?>
+    </body>
+</html>
